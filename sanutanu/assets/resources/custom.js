e52acec7a00cd04e19cb2672aad446983c04168c
bilal waheed <chbilalwaheed@gmail.com>
2020-01-01 15:07:36 +0500

    SignUpandLoginValidations
    SignUp and Login Validations with popover

--- sanutanu/assets/resources/custom.js
+++ sanutanu/assets/resources/custom.js
@@ -7,10 +7,154 @@ function registeration_submit(id){
 	}
 }
 
-$("form#registration_form input,select").on("keyup change",function(){
-	signup_validations();
-});
+function Form_validations(F_data)
+{
+	debugger;
+	var Password_rxp = /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+	var Name_RXP = /^[a-zA-Z\s ]{3,50}$/;
+	var Email_RXP = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var InputName = F_data.name;
+var InputValue = F_data.value;
+var InputId = F_data.id;
+console.log(InputId);
+if(InputName=="user_first_name")
+  {
+	if((InputValue==null || InputValue==""))
+       {
+       	document.getElementById('user_first_name').style.borderColor='red';
+	 $('#user_first_name').popover('enable');
+	 $('#user_first_name').popover('show');
+	      return false;
+        }
+         else if(!(InputValue.match(Name_RXP)))
+         	    {
+         	    		document.getElementById('user_first_name').style.borderColor='red';
+         	    		$('#user_first_name').popover();
+         	    } 
+         	    else 
+         	    {
+         	    	document.getElementById('user_first_name').style.borderColor='';
+         	    		$('#user_first_name').popover("disable");
+         	    		return true;
+         	    }
+
+  } else if(InputName=="user_last_name")
+               {
+  	        if((InputValue==null || InputValue==""))
+                     {
+       	document.getElementById('user_last_name').style.borderColor='red';
+	 $('#user_last_name').popover();
+	      return false;
+                     }
+         else if(!(InputValue.match(Name_RXP)))
+         	    {
+         	    		document.getElementById('user_last_name').style.borderColor='red';
+         	    		$('#user_last_name').popover();
+         	    }
+         	    else 
+         	    {
+         	    	document.getElementById('user_last_name').style.borderColor='';
+         	    		$('#user_last_name').popover("disable");
+         	    		return true;
+         	    }
+  }
+     else if(InputName=="user_email")
+     { if(InputId=="user_email_Login")
+ {
+     	if((InputValue==null || InputValue==""))
+                     {
+       	document.getElementById('user_email_Login').style.borderColor='red';
+	 $('#user_email_Login').popover();
+	      return false;
+                     }
+                     else if(!(InputValue.match(Email_RXP)))
+                             {
+                             	document.getElementById('user_email_Login').style.borderColor='red';
+	                             $('#user_email_Login').popover();
+                             }else 
+         	    {
+         	    	document.getElementById('user_email_Login').style.borderColor='';
+         	    		$('#user_email_Login').popover("disable");
+         	    		return true;
+         	    }
+}else if(InputId="user_email")
+{
+if((InputValue==null || InputValue==""))
+                     {
+       	document.getElementById('user_email').style.borderColor='red';
+	 $('#user_email').popover();
+	      return false;
+                     }
+                     else if(!(InputValue.match(Email_RXP)))
+                             {
+                             	document.getElementById('user_email').style.borderColor='red';
+	                             $('#user_email').popover();
+                             }else 
+         	    {
+         	    	document.getElementById('user_email').style.borderColor='';
+         	    		$('#user_email').popover("disable");
+         	    		return true;
+         	    }	
+}
+     } else if(InputName=="user_password")
+       {
+       	if(InputId=="user_password_Log")
+       	{
+       	if((InputValue==null || InputValue==""))
+                     {
+       	document.getElementById('user_password_Log').style.borderColor='red';
+	 $('#user_password_Log').popover();
+	      return false;
+                     }else if(!(InputValue.match(Password_rxp)) )
+                     {
+                     		document.getElementById('user_password_Log').style.borderColor='red';
+	                        $('#user_password_Log').popover();
+	                             return false;
+                     } 
+         	    {
+         	    	document.getElementById('user_password_Log').style.borderColor='';
+         	    		$('#user_password_Log').popover("disable");
+         	    		return true;
+         	    } 
+                     
+       } else if(InputId="user_password")
+       {
+       	if((InputValue==null || InputValue==""))
+                     {
+       	document.getElementById('user_password').style.borderColor='red';
+	 $('#user_password').popover();
+	      return false;
+                     }else if(!(InputValue.match(Password_rxp)) )
+                     {
+                     		document.getElementById('user_password').style.borderColor='red';
+	                        $('#user_password').popover();
+	                             return false;
+                     } 
+         	    {
+         	    	document.getElementById('user_password').style.borderColor='';
+         	    		$('#user_password').popover("disable");
+         	    		return true;
+         	    } 
+       }
+}
+
+}
+function Remove_Validations()
+{
+	debugger;
+	    document.getElementById('user_first_name').style.borderColor='';
+		document.getElementById('user_last_name').style.borderColor='';
+		document.getElementById('user_email').style.borderColor='';
+		document.getElementById('user_password').style.borderColor='';
+		document.getElementById('day').style.borderColor='';
+		document.getElementById('month').style.borderColor='';
+		document.getElementById('year').style.borderColor='';
+						  $('[data="popover"]').popover("hide");
+						  $('[data="popover"]').popover("dispose");
+
+}
 function signup_validations(){
+
 	var user_first_name = $("form#registration_form #user_first_name").val();
 	var user_last_name = $("form#registration_form #user_last_name").val();
 	var user_email = $("form#registration_form #user_email").val();
@@ -20,10 +164,23 @@ function signup_validations(){
 	var year = $("form#registration_form #year").val();
 	var user_gender = $("form#registration_form input[name=user_gender]:checked").val();
 	if(user_first_name == "" || user_last_name == "" || user_email == "" || user_password == "" || day == "" || month == "" || year == ""){
-		$('[data="popover"]').popover("show");
+		  document.getElementById('user_first_name').style.borderColor='red';
+				document.getElementById('user_last_name').style.borderColor='red';
+					document.getElementById('user_email').style.borderColor='red';
+						document.getElementById('user_password').style.borderColor='red';
+						document.getElementById('day').style.borderColor='red';
+						document.getElementById('month').style.borderColor='red';
+						document.getElementById('year').style.borderColor='red';
+						  $('[data="popover"]').popover("show");
+
 		return false;
+
+
 	}else{
-		$('[data="popover"]').popover("hide");
+		                 document.getElementById('day').style.borderColor='';
+						document.getElementById('month').style.borderColor='';
+						document.getElementById('year').style.borderColor='';
+		$('[data="popover"]').popover("disable");
 		return true;
 	}
 }
