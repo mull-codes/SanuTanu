c36d0f4c71cab451e50ed216ff3e870c0815d307
bilal waheed <chbilalwaheed@gmail.com>
2020-01-02 15:17:29 +0500

    phoneNumberIs required
    Phone number is added

--- sanutanu/assets/resources/custom.js
+++ sanutanu/assets/resources/custom.js
@@ -1,55 +1,69 @@
 var base_url = $('#base_url').val();
-
 function registeration_submit(id){
+	var country_code = $('div#User_PhoneNumber button#btn-country span.btn-cc').text();
+	var num = document.getElementById('phoneNumber').value;
+	alert(country_code);
+	alert(num);
 	var valid = signup_validations();
 	if(valid == true){
 		ajaxSubmit(id,"Data Submited");	
 	}
 }
+$('#phoneNumber').on('keypress', function(ev) {
+	debugger;
+    var keyCode = window.event ? ev.keyCode : ev.which;
+    //codes for 0-9
+    if (keyCode < 48 || keyCode > 57) {
+        //codes for backspace, delete, enter
+        if (keyCode != 0 && keyCode != 8 && keyCode != 13 && !ev.ctrlKey) {
+            ev.preventDefault();
+        }
+    }
+});
 
- $("ul#list1").on('click',function(){
-debugger;
-var id =$("#list1 li.active").attr('id');
+// LIst check list register or login validations removel
 
-if(id=="register1")
-{
-	document.getElementById('user_first_name').style.borderColor='';
-         	    		$('#user_first_name').popover("hide");
-    document.getElementById('user_last_name').style.borderColor='';
-         	    		$('#user_last_name').popover("hide");
-    document.getElementById('user_email').style.borderColor='';
-         	    		$('#user_email').popover("hide");
-    document.getElementById('user_password').style.borderColor='';
-         	    		$('#user_password').popover("hide"); 
-    document.getElementById('day').style.borderColor='';
-	document.getElementById('month').style.borderColor='';
-	document.getElementById('year').style.borderColor='';    	    		     	    		
-	$('#day').popover("hide");$('#month').popover("hide");
-	$('#year').popover("hide");
-}else if(id=="login1")
-{
-	document.getElementById('user_email_Login').style.borderColor='';
-         	    		$('#user_email_Login').popover("hide");
-    document.getElementById('user_password_Log').style.borderColor='';
-         	    		$('#user_password_Log').popover("hide");
-}
+$("ul#list1").on('click',function(){
+	var id =$("#list1 li.active").attr('id');
 
- });
+	if(id=="register1"){
+		document.getElementById('user_first_name').style.borderColor='';
+	         	    		$('#user_first_name').popover("hide");
+	    document.getElementById('user_last_name').style.borderColor='';
+	         	    		$('#user_last_name').popover("hide");
+	    document.getElementById('user_email').style.borderColor='';
+	         	    		$('#user_email').popover("hide");
+	    document.getElementById('user_password').style.borderColor='';
+	         	    		$('#user_password').popover("hide"); 
+	    document.getElementById('day').style.borderColor='';
+		document.getElementById('month').style.borderColor='';
+		document.getElementById('year').style.borderColor='';    	    		     	    		
+		$('#day').popover("hide");$('#month').popover("hide");
+		$('#year').popover("hide");
+	}else if(id=="login1"){
+		document.getElementById('user_email_Login').style.borderColor='';
+	         	    		$('#user_email_Login').popover("hide");
+	    document.getElementById('user_password_Log').style.borderColor='';
+	         	    		$('#user_password_Log').popover("hide");
+	}
 
+});
 
+// validations function for register and login forms on foucus out 
   
-function Form_validations(F_data)
-{
+function Form_validations(F_data){
 	debugger;
 	var Password_rxp = /^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
 	var Name_RXP = /^[a-zA-Z\s ]{3,50}$/;
 	var Email_RXP = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	var InputName = F_data.name;
-var InputValue = F_data.value;
-var InputId = F_data.id;
-console.log(InputId);
+	var InputValue = F_data.value;
+	var InputId = F_data.id;
+// chek for user first name 
+// it will check field is empty or pattren
 if(InputName=="user_first_name")
   {
+
 	if((InputValue==null || InputValue==""))
        {
        	document.getElementById('user_first_name').style.borderColor='red';
@@ -69,7 +83,8 @@ if(InputName=="user_first_name")
          	    		$('#user_first_name').popover("disable");
          	    		return true;
          	    }
-
+// chek for user last name 
+// it will check field is empty or pattren
   } else if(InputName=="user_last_name")
                {
   	        if((InputValue==null || InputValue==""))
@@ -93,6 +108,8 @@ if(InputName=="user_first_name")
          	    		return true;
          	    }
   }
+  // chek for user email for login
+// it will check field is empty or pattren
      else if(InputName=="user_email")
      { if(InputId=="user_email_Login")
  {
@@ -114,6 +131,8 @@ if(InputName=="user_first_name")
          	    		$('#user_email_Login').popover("disable");
          	    		return true;
          	    }
+         	     // chek for user email for rigisteration 
+// it will check field is empty or pattren
 }else if(InputId="user_email")
 {
 if((InputValue==null || InputValue==""))
@@ -135,8 +154,11 @@ if((InputValue==null || InputValue==""))
          	    		return true;
          	    }	
 }
+
      } else if(InputName=="user_password")
        {
+       	 // chek for user password for login 
+// it will check field is empty or pattren
        	if(InputId=="user_password_Log")
        	{
        	if((InputValue==null || InputValue==""))
@@ -157,7 +179,8 @@ if((InputValue==null || InputValue==""))
          	    		$('#user_password_Log').popover("disable");
          	    		return true;
          	    } 
-                     
+                   // chek for user password for rigisterations
+// it will check field is empty or pattren   
        } else if(InputId="user_password")
        {
        	if((InputValue==null || InputValue==""))
@@ -183,9 +206,10 @@ if((InputValue==null || InputValue==""))
 }
 
 }
+// function for rmovel of validations on input when user start typing during error
+// it will remove all validations errors from all fields 
 function Remove_Validations()
 {
-	debugger;
 	    document.getElementById('user_first_name').style.borderColor='';
 		document.getElementById('user_last_name').style.borderColor='';
 		document.getElementById('user_email').style.borderColor='';
@@ -197,6 +221,8 @@ function Remove_Validations()
 						  $('[data="popover"]').popover("dispose");
 
 }
+// this function work when sing up button hit
+// it will show error on every feilds 
 function signup_validations(){
 
 	var user_first_name = $("form#registration_form #user_first_name").val();
